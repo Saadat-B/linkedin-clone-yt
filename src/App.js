18456f7a844af262792d29cfdb85899a44f334c6
Saadat Badgujar <saadatbadgujar@gmail.com>
2022-12-01 09:54:15 +0530

    Secocond commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 import "./App.css";
-
+import Feed from "./Feed";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 function App() {
   return (
-    <div className="App">
-      <h1>Let's build a Linkedin CLONE</h1>
+    <div className="app">
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+        <Feed />
+      </div>
+      {/* {Widgets} */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBGgbiXv_A0oUakj0L_6y4nPfnkvggpyyU",
+  authDomain: "linkedin-clone-yt-4f772.firebaseapp.com",
+  projectId: "linkedin-clone-yt-4f772",
+  storageBucket: "linkedin-clone-yt-4f772.appspot.com",
+  messagingSenderId: "662912370494",
+  appId: "1:662912370494:web:182437be98bd96b9dd8e6f",
+  measurementId: "G-PL312JLR24",
+};
+
+const app = initializeApp(firebaseConfig);
